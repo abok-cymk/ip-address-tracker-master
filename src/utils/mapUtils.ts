96c9dace0cf596727dcc2cea203a142e9c9599fe
abok-cymk <allanabok18@gmail.com>
2025-08-14 14:58:04 +0300

    commented out unused defaultIcon fn

--- src/utils/mapUtils.ts
+++ src/utils/mapUtils.ts
@@ -18,18 +18,18 @@ export function createMapIcon(): L.Icon {
   });
 
   // Default Leaflet icon as fallback
-  const defaultIcon = new L.Icon({
-    iconUrl:
-      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png",
-    iconRetinaUrl:
-      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png",
-    shadowUrl:
-      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41],
-  });
+//   const defaultIcon = new L.Icon({
+//     iconUrl:
+//       "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png",
+//     iconRetinaUrl:
+//       "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png",
+//     shadowUrl:
+//       "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
+//     iconSize: [25, 41],
+//     iconAnchor: [12, 41],
+//     popupAnchor: [1, -34],
+//     shadowSize: [41, 41],
+//   });
 
   // Check if custom icon is available
   const img = new Image();
